7fb22da00bcb820052dfeacb4ad94ea57c6ac15b
wqatsy <wangquan_pt@sina.com>
2022-06-02 07:49:42 +0800

    Add Listing 2.1 The first.c Program

---
+++ Listing 2.1 The first.c Program/first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(viod)
+{
+    int num;
+    num = 1;
+
+    printf("I am a simple ");
+    printf("computer.\n");
+    printf("My favorite number is %d because it is first.\n", num);
+
+    printf("Press any key to exit.");
+    getch();
+
+    return 0;
+}
