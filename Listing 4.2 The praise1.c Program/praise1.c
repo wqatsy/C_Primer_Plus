092542d69d267ef9dde7533f51e981ef1f3c6a41
wqatsy <neverdown1990@sina.com>
2022-06-17 09:40:25 +0800

    Add Listing 4.2 The praise1.c Program

---
+++ Listing 4.2 The praise1.c Program/praise1.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:6031)
+
+/* praise1.c -- uses an assortment of strings */
+#include <stdio.h>
+
+#define PRAISE "You are an extraordinary being."
+
+int main(void)
+{
+    char name[40];
+
+    printf("What's your name?");
+    scanf("%s", name);
+    printf("Hello, %s. %s\n", name, PRAISE);
+
+    printf("Press any key to exit.");
+    getch();
+    return 0;
+}
