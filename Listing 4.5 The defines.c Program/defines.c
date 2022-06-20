a359528159e4ab392dd8744baaf899206cebb5db
wqatsy <neverdown1990@sina.com>
2022-06-20 22:28:06 +0800

    Add Listing 4.5 The defines.c Program

---
+++ Listing 4.5 The defines.c Program/defines.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+// defines.c -- uses defined constants from limit.h and float.
+#include <stdio.h>
+#include <limits.h> // integer limits
+#include <float.h> // floating-point limits
+
+int main(void)
+{
+    printf("Some number limits for this system:\n");
+    printf("Biggest int: %d\n", INT_MAX);
+    printf("Smallest long long: %lld\n", LLONG_MIN);
+    printf("One byte = %d bits on this system.\n", CHAR_BIT);
+    printf("Largest double: %e\n", DBL_MAX);
+    printf("Smallest normal float: %e\n", FLT_MIN);
+    printf("float precision = %d digits\n", FLT_DIG);
+    printf("float epsilon = %e\n", FLT_EPSILON);
+
+    printf("Press any key to exit.");
+    getch();
+    return 0;
+}
