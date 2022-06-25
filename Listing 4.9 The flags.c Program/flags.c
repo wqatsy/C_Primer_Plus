247d6520a1d5610a26fbccb2fab8097020f8bbb2
wqatsy <neverdown1990@sina.com>
2022-06-25 22:12:15 +0800

    Add Listing 4.9 The flags.c Program

---
+++ Listing 4.9 The flags.c Program/flags.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+/* flags.c -- illustrates some formatting flags */
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%x %X %#x\n", 31, 31, 31);
+    printf("**%d**% d**% d**\n", 42, 42, -42);
+    printf("**%5d**%5.3d**%05d**%05.3d**\n", 6, 6, 6, 6);
+
+    printf("Press any key to exit.");
+    getch();
+    return 0;
+}
