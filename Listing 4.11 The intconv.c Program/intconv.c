dba953bf85be3603009b259d724961b9149e8536
wqatsy <neverdown1990@sina.com>
2022-06-27 00:03:09 +0800

    Add Listing 4.11 The intconv.c Program

---
+++ Listing 4.11 The intconv.c Program/intconv.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+/* intconv.c -- some mismatched integer conversions */
+#include <stdio.h>
+
+#define PAGES 336
+#define WORDS 65618
+
+int main(void)
+{
+    short num = PAGES;
+    short mnum = -PAGES;
+
+    printf("num as short and unsigned short: %hd %hu\n", num, num);
+    printf("-num as short and unsigned short: %hd %hu\n", mnum, mnum);
+    printf("num as int and char: %d %c\n", num, num);
+    printf("WORDS as int, short, and char: %d %hd %c\n", WORDS, WORDS, WORDS);
+
+    printf("Press any key to exit.");
+    getch();
+    return 0;
+}
